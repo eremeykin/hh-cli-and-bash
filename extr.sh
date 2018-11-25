95405e65933c36f7aaba0e7f7bea036c3d8bf181
Petr Eremeykin <eremeykin@gmail.com>
2018-11-25 22:03:57 +0300

    Create extr.sh to extract data from log entries

---
+++ extr.sh
@@ -0,0 +1,19 @@
+function extr {
+    regex="\[404\]\s[0-9]+\s:([0-9]+)\s(.*)\s=([0-9]+)"
+
+    while read line
+    do
+        if [[ $line =~ $regex ]]
+        then
+            permissions="${BASH_REMATCH[1]}"
+            path="${BASH_REMATCH[2]}"
+            size="${BASH_REMATCH[3]}"
+            echo "${permissions}"
+            echo "${path}"
+            echo "${size}"
+        else
+            echo "${line} doesn't match" >&2
+            exit -1
+        fi
+    done < "${1:-/dev/stdin}"
+}
