d4d09ee930b69b206dbd4c8d1fdbccefd5af8a94
Petr Eremeykin <eremeykin@gmail.com>
2018-11-30 18:20:50 +0300

    Task 3 complete

--- main.sh
+++ main.sh
@@ -31,18 +31,53 @@ function gkey {
     echo $line
 }
 
+function gxrid {
+    line=`script -c "$1" | grep X-Request-ID | xargs -0 printf "%q"`
+    line=${line#*[33m}
+    line=${line%%\'*}
+    echo $line
+}
+
 # init tsk
 key=`gkey "tsk -k eremeykin@gmail.com"`
 echo "Initial key:" $key
 
 # init task 1
-script -c "tsk -s 1 -k $key" > /dev/null
+tsk -s 1 -k $key
 rm -rf ~/"-r"
 key=`gkey "tsk -s 1 -k $key -c"`
 echo "Task 1  key:" $key
 
 # init task 2
-script -c "tsk -s 2 -k $key" > /dev/null
+tsk -s 2 -k $key
 cat /var/log/loggen/*.log | extr
 key=`gkey "tsk -s 2 -k $key -c"`
 echo "Task 2  key:" $key
+
+# init task 3
+tsk -s 3 -k $key
+pid=`ps -a | grep sigproc`
+pid=${pid% pts*}
+echo $pid
+
+while [ ! -f '/var/log/challenge/done.key' ]
+do
+    kill -s SIGUSR1 $pid && kill -s SIGUSR2 $pid && kill -s SIGINT $pid
+done
+
+ln -s /var/log/challenge/done.key
+key=`gkey "tsk -s 3 -k $key -c"`
+echo "Task 3  key:" $key
+
+# init task 4
+# set -x
+# tsk -s 4 -k $key
+# xrid=`gxrid "tsk -s 4 -k $key"`
+# echo $xrid 
+# cred=`curl localhost:9182/task1 -H "X-Request-ID:$xrid" -v 2>&1 | grep X-Credentials:`
+# echo $cred
+# cred=${cred#*:}
+# echo $cred
+# curl -L localhost:9182/task2 -d "credentials=$cred;" -v
+
+
